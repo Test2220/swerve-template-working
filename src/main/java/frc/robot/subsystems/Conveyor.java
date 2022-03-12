ddc5575fd77427ff4e9accb7050ea220f5f639bd
Ethan Krech <ethankrech@gmail.com>
2022-03-12 11:48:15 -0600

    Conveyor

--- src/main/java/frc/robot/subsystems/Conveyor.java
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -14,10 +14,15 @@ public class Conveyor extends SubsystemBase {
     PhotoEyeSensor photoEyeSensorIn = new PhotoEyeSensor(PHOTOEYE_SENSOR_INTAKE, true);
     PhotoEyeSensor photoEyeSensorOut = new PhotoEyeSensor(PHOTOEYE_SENSOR_LAUNCHER, true);
     TalonFX talon = new TalonFX(CONVEYOR_FALCON);
+    int inRobot = 0;
+    boolean ballInput = false;
+    boolean ballOutput = false;
 
     public Conveyor() {
         Shuffleboard.getTab("Conveyor").addBoolean("Sensor In", photoEyeSensorIn::get);
         Shuffleboard.getTab("Conveyor").addBoolean("Sensor Out", photoEyeSensorOut::get);
+        Shuffleboard.getTab("Conveyor").addNumber("In Robot", () -> inRobot);
+        Shuffleboard.getTab("Conveyor").addBo
 
         talon.setInverted(true);
     }
@@ -28,10 +33,34 @@ public class Conveyor extends SubsystemBase {
 
     public boolean isBallPresentAtInput() {
         return photoEyeSensorIn.get();
+
     }
 
     public boolean isBallPresentAtShooter() {
         return photoEyeSensorOut.get();
+    }
+
+    @Override
+    public void periodic(){
+        if (isBallPresentAtInput()) {
+            if (!ballInput) {
+                inRobot++;
+                ballInput = true;
+            }
+        } else {
+            ballInput = false;
+        }
+
+        if (isBallPresentAtShooter()) {
+            if (!ballOutput) {
+                inRobot--;
+                ballOutput = true;
+            }
+        } else {
+            ballOutput = false;
+        }
+
+        
     }
 
 }
