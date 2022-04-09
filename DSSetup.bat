7383581f10b0652581eb9a2982f84a2ecb217718
Thanuboyapati <Thanu.boyapati@gmail.com>
2022-04-08 20:35:39 -0500

    Update DSSetup.bat

--- DSSetup.bat
+++ DSSetup.bat
@@ -3,10 +3,12 @@ taskkill /IM "DriverStation.exe"
 ::Force close shuffleboard so there is no save dialog, which prevents it from restarting
 taskkill /IM "javaw.exe" /F
 
+taskkill /IM "electron.exe"
+
 :: 2 seconds is the minimum, 1.9 doesn't allow time to close and reopen
 timeout /t 2
 
-Start "" "C:\Program Files (x86)\FRC Driver Station\DriverStation.exe"
+Start /max "Blue Twighlight" "C:\Program Files (x86)\FRC Driver Station\DriverStation.exe"
 
 cd "C:\Users\User\Documents\GitHub\swerve-template-working\TwighlightDash"
 npm run start
