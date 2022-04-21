fa53d7f33f1d6fbb0a9383e593c0cc16baa9b3b6
Thanuboyapati <Thanu.boyapati@gmail.com>
2022-04-21 18:15:40 -0500

    hopefully final auto changes

--- src/main/java/frc/robot/autopaths/ReferenceATwoBall.java
+++ src/main/java/frc/robot/autopaths/ReferenceATwoBall.java
@@ -5,13 +5,11 @@ import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Constants;
 import frc.robot.FieldConstants;
 import frc.robot.commands.ExtendIntake;
 import frc.robot.commands.GoToCommand;
 import frc.robot.commands.RetractIntake;
 import frc.robot.commands.RunIntake;
-import frc.robot.commands.RunShooter;
 import frc.robot.commands.RunShooterVelocity;
 import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Drivetrain;
@@ -40,7 +38,7 @@ public class ReferenceATwoBall extends SequentialCommandGroup {
                         ))
             )).raceWith(new RunIntake(intake, false)),
             
-            new GoToCommand(drivetrain, GeomUtil.getRobotCoordinate(FieldConstants.referenceAShootRobotCenter).transformBy(new Transform2d(new Translation2d(), Rotation2d.fromDegrees(-6.25)))).raceWith(new RunIntake(intake, false)),
+            new GoToCommand(drivetrain, GeomUtil.getRobotCoordinate(FieldConstants.referenceA).transformBy(new Transform2d(new Translation2d(), Rotation2d.fromDegrees(-6.25)))).raceWith(new RunIntake(intake, false)),
 
             new RetractIntake(intake),
 
